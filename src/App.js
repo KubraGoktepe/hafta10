ebe687c198d7ff328dc20e1ab32b6f2e400436d1
KubraGoktepe <kbrckra@gmail.com>
2024-03-17 20:41:22 +0300

    useeffect fetch örn

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import { useEffect, useState } from "react";
-import C1 from "./C1";
+import { useState } from "react";
+import UlkeListe from "./UlkeListe";
 
 function App() {
-  const [c1Kapali, c1AcKapa] = useState(false);
-
-  function c1Kapat() {
-    c1AcKapa((eskiDeger) => !eskiDeger);
-  }
-
   return (
     <>
-      <div className="container my-5 bg-info-subtle p-5">
-        <button onClick={c1Kapat} className="btn btn-warning btn-sm my-3">
-          C1 KAPAT
-        </button>
-
-        {c1Kapali && <C1 />}
+      <div className="container my-3">
+        <UlkeListe />
       </div>
     </>
   );
